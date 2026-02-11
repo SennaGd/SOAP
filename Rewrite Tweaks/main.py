516c16312d2c8bc6e130ec9730054f7c2addfe1d
Senna :3 <sd@tapp.online>
2026-02-11 22:33:07 +0100

    Able to create dictionary containing a tweak name and all of it's registry edits.

---
+++ Rewrite Tweaks/main.py
@@ -0,0 +1,65 @@
+import os, json, re
+
+
+# (1). Read File (open file)
+# (2). Split contents (only needed info)
+# (3). Split the split contents (Hive, Path, etc.) 
+#? ^^^ -> Reading Data
+
+# (1). Traverse folders
+# (2). Find File -------------------|
+# (3). Save FileName                |
+# (4). Search FilePath of the File--|
+#? ^^^ -> Getting path & name
+
+# (1). Write the split data into tweaks.json file
+#? ^^^ -> Writing Data
+
+
+tweakPath = "./testfile.bat"
+
+pattern = re.compile(
+    r'^reg(?:\.exe)?\s+(add|delete)\s+"((HKLM|HKCU|HKCR|HKU|HKCC|'
+    r'HKEY_LOCAL_MACHINE|HKEY_CURRENT_USER|HKEY_CLASSES_ROOT|'
+    r'HKEY_USERS|HKEY_CURRENT_CONFIG)\\([^"]+))"'
+    r'(?:\s+/v\s+"([^"]+)")?'
+    r'(?:\s+/t\s+(REG_\w+))?'
+    r'(?:\s+/d\s+"?([^"]+)"?)?'
+    r'(?:\s+/f)?$',
+    re.IGNORECASE
+)
+i = 0
+with open(tweakPath, "r") as file:
+    name = file.name.split('./')[-1]
+    content = file.readlines()
+
+print(name.split('./')[-1], ": Contents")
+tweak = {name : []}        
+
+for line in content:
+    regexSearch = re.match(pattern, line)
+    if regexSearch:
+        function = regexSearch.group(1)
+        hive = regexSearch.group(3)
+        path = regexSearch.group(4)
+        name = regexSearch.group(5)
+        value = regexSearch.group(6)
+        path = path.replace("\\", "\\\\")
+        # print("\nfunction: ", function)
+        # print("hive: ", hive)
+        # print("path: ", path)
+        # print("name: ", name)
+        # print("value ", value)
+        
+        tweak[list(tweak)[0]].append({
+            'function':function,
+            'hive':hive,
+            'path':path,
+            'name':name,
+            'value':value
+        })
+        
+for x in tweak:
+    for y in tweak[x]:
+        print(y)
+        
---
+++ Rewrite Tweaks/plan.md
@@ -0,0 +1,26 @@
+## Rewrite tweaks into database
+
+### What is a tweak?
+A tweak is a sequence of registry changes
+that changes the way the system behaves. 
+This can be changing a registry key from value 1 -> 0,
+which will as an example disable a power saving feature.
+
+### What categories does a tweak have?
+A tweak can be split into multiple fragments,
+one tweak alone can and will have multiple tweaks inside it.
+But thats for later,
+
+
+
+### How to obtain the name of the tweak?
+
+
+### How will the value be stored?
+
+
+### How will the tweak be split?
+
+
+### How to handle multiple registry changes?
+
